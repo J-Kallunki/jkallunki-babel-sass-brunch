ca49006f5fb53307bf4c6d79746bd28729597cd7
Joonas Kallunki <jkallunki@gmail.com>
2017-12-11 07:31:35 +0200

    Fix normalize import

--- brunch-config.js
+++ brunch-config.js
@@ -1,61 +1,67 @@
 exports.config = {
   conventions: {
     assets: /src\/assets\//,
-    ignored: /\.test\.\w+$/
+    ignored: /\.test\.\w+$/,
   },
   sourceMaps: true,
 
   paths: {
-    public: "public",
-    watched: ["src", "vendor"]
+    public: 'public',
+    watched: ['src', 'vendor'],
+  },
+
+  npm: {
+    enabled: true,
+    styles: {
+      'normalize.css': ['normalize.css'],
+    },
   },
 
   files: {
     javascripts: {
       joinTo: {
         // all code from 'src/'
-        "js/main.js": /^src/,
+        'js/main.js': /^src/,
         // all code from 'vendor/' and imported node_modules
-        "js/vendor.js": /^(vendor\/|node_modules\/)/
-      }
+        'js/vendor.js': /^(vendor\/|node_modules\/)/,
+      },
     },
-    stylesheets: { joinTo: "css/styles.css" }
+    stylesheets: {
+      joinTo: { 'css/styles.css': /^src/, 'css/vendor.css': /^(?!src)/ },
+    },
   },
 
   stylelint: {
-    pattern: "^src[\\/\\\\].*\\.scss?$"
+    pattern: '^src[\\/\\\\].*\\.scss?$',
   },
 
   plugins: {
     sass: {
-      mode: "native",
-      options: {
-        includePaths: ["node_modules/normalize.css"]
-      },
+      mode: 'native',
       // Set the precision for arithmetic operations.
       precision: 8,
       // To enable embedded source maps, pass the option `sourceMapEmbed`. This is only supported in _native_ mode; Ruby Sass isn't supported.
-      sourceMapEmbed: true
+      sourceMapEmbed: true,
     },
     pleeease: {
       sass: false,
       autoprefixer: {
-        browsers: ["last 3 versions", "> 1%"]
+        browsers: ['last 3 versions', '> 1%'],
       },
       filters: { oldIE: false },
-      rem: ["16px", { replace: false, atrules: false }],
+      rem: ['16px', { replace: false, atrules: false }],
       pseudoElements: true,
       opacity: true,
       import: false,
       minifier: { preserveHacks: true, removeAllComments: true },
       mqpacker: true,
-      sourcemaps: true
-    }
+      sourcemaps: true,
+    },
   },
 
   modules: {
     autoRequire: {
-      "js/main.js": ["src/js/initialize"]
-    }
-  }
+      'js/main.js': ['src/js/initialize'],
+    },
+  },
 };
